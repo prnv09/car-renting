6ebd83dc4578ee32c44c0b89a8b01454b0246027
prnv09 <pranavkumbhar727@gmail.com>
2022-11-08 22:23:19 +0530

    function aded

--- carRenting.sol
+++ carRenting.sol
@@ -7,15 +7,45 @@
 
 pragma solidity ^0.8.9;
 contract rent{
-    string[3] car = ["Honda", "BMW", "Audi"];
+    string[3] car = ["1:Honda", "2:BMW", "3:Audi"];
      address payable public owner;
     uint startTime;
-    enum carStatus 
+    uint totalTime;
+    enum carStatus{available, rented}
+    carStatus public HondaStatus;
+    carStatus public BMWStatus;
+    carStatus public AudiStatus;
+    mapping (address => bytes32)userStatus;
     function CarList() public view returns (string[3] memory ){
         return car;
     }
 
-    function RentCar(string memory _carname) public {
+    function RentCar(bytes32 _carname) public {
+        if (_carname == "Honda"){
+            HondaStatus=carStatus.rented;
+        }
+        if (_carname == "BMW"){
+            BMWStatus=carStatus.rented;
+        }
+        if (_carname == "Audi"){
+            AudiStatus=carStatus.rented;
+        }
+        startTime = block.timestamp;
+        userStatus[msg.sender]= _carname;
+    }
+
+    function stopCar(bytes32 _carname) public returns(uint){
+        require(userStatus[msg.sender]==_carname,"You not have rented this car");
+        totalTime=(block.timestamp - startTime)/60;
+        return totalTime;
+
+    }
+
+    function LeaveCar(bytes32 _carname) public payable {
+        require(userStatus[msg.sender]==_carname,"You not have rented this car");
+        // totalTime=(block.timestamp - startTime)/60;
+        require(msg.value==totalTime,"Please enter correct amount");
+        
 
     }
 
