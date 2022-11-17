ad2b6162c9e20e01c166ce5c4078127f7c82b8c9
prnv09 <pranavkumbhar727@gmail.com>
2022-11-17 14:24:14 +0530

    some changes

--- carRenting.sol
+++ carRenting.sol
@@ -8,17 +8,19 @@ contract rent is alpha{
     }
     //STATE VARIABLES ===========================================================================================
     address payable public owner;
-    uint startTime;
-    uint totalTime;
+    // uint startTime;
+    // uint totalTime;
     enum carStatus{available, rented}
-    uint totalRent;
+    // uint totalRent;
 
-    //CAR STRUCTURES ===========================================================================================
+//CAR STRUCTURES ===========================================================================================
     struct car {
         uint carID;
         string carName;
         address rentedBy;
         uint startTime;
+        uint totalTime;
+        uint totalRent;
         carStatus checkAvailability;
     }
     struct carInfo{
@@ -28,18 +30,17 @@ contract rent is alpha{
   
     carInfo[] carList;
 
-    //MAPPING & MODIFIER =====================================================================================================
-    mapping (address => car)userStatus;
+//MAPPING & MODIFIER =====================================================================================================
     mapping(uint =>  car) carMapping;
     modifier notOwner(){
        require(owner != msg.sender,"Owner can not rent his own car");
         _;
     }
 
-    //EVENTS ==================================================================================================
+//EVENTS ======================================================================================================
     event carRented(address indexed renter,string name,uint id);
-    event carParked(address indexed renter,string name,uint id,uint rent,uint time);
     event rentPaid(address indexed renter,string name,uint id, uint rent);
+    event carAdded (string carName, uint carID);
 
 //OWNER CAN ADD CAR ==============================================================================================
     function addCar(string memory _carName,uint _carID)public {
@@ -47,16 +48,20 @@ contract rent is alpha{
         carInfo memory _carInfo;
         c.carName=_carName;
         c.carID=_carID;
+        // c.checkAvailability = carStatus.available;
         _carInfo.carName=_carName;
         _carInfo.carID=_carID;
-        c.checkAvailability = carStatus.available;
         carList.push(_carInfo);
         carMapping[_carID] = c;
+        emit carAdded(_carName,_carID);
     }
+
+//USER CAN SEE CAR LIST HERE ===================================================================================
     function CarList() public view returns (carInfo[] memory ){
         return carList;
     }
     
+//USER CAN RENT A CAR HERE =====================================================================================
     function RentCar(uint _carID) public notOwner {
 
         require(carMapping[_carID].checkAvailability == carStatus.available,"This car is already rented by someone");
@@ -66,26 +71,23 @@ contract rent is alpha{
         emit carRented(msg.sender,carMapping[_carID].carName,_carID);
     }
 
-    function checkRent(uint _carID) public returns(uint){
+//USER CAN CHECK HIS RENT HERE  =========================================================================
+    function CheckRent(uint _carID) public returns(uint){
         require(carMapping[_carID].rentedBy == msg.sender,"This car is not rented by you");
-        totalTime = (block.timestamp - carMapping[_carID].startTime)/60;
-        // ether payment feature 
-        // totalRent = (totalTime*1000000000000000000);
-        totalRent = totalTime;
-        emit carParked(msg.sender,carMapping[_carID].carName,_carID,totalRent,totalTime);
-        return totalRent;
+        carMapping[_carID].totalRent = (block.timestamp - carMapping[_carID].startTime)/60;
+        return carMapping[_carID].totalRent;
     }
 
+
+
+//USER CAN PAY RENT AND LEAVE THE CAR ==============================================================================
     function payRent(uint _carID,uint _rent) public payable {
          require(carMapping[_carID].rentedBy == msg.sender,"This car is not rented by you");
-         require(_rent == totalRent,"Please enter correct amount of ether");
-
-        //  owner.transfer(msg.value);
+         require(_rent == carMapping[_carID].totalRent,"Please enter correct amount of ether");
          transfer(owner,_rent);
          carMapping[_carID].checkAvailability = carStatus.available;
          carMapping[_carID].rentedBy = address(0);
-         emit rentPaid(msg.sender,carMapping[_carID].carName,_carID,totalRent);
-         
+         emit rentPaid(msg.sender,carMapping[_carID].carName,_carID,carMapping[_carID].totalRent);
     }
 
 
